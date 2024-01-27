9310c8261c855730d56f8c2e80e97ee2ef41f34f
Edgar Flores <edgargdl1986@gmail.com>
2024-01-27 11:16:54 -0600

    cleanup

--- HomeScreen.js
+++ HomeScreen.js
@@ -1,9 +1,8 @@
-import { View, Text, Image, TextInput, ScrollView, SafeAreaView } from 'react-native'
+import { SafeAreaView } from 'react-native'
 import React, { useEffect, useLayoutEffect, useState  } from 'react';
 import { useNavigation } from '@react-navigation/native';
-import Programs from './programs';
 import Runprogram from './runprogram';
-import Excersices from './excercises';
+
 
 
 const HomeScreen = () => {
